50278b656aa3f9018927bc1121949010a76ce46e
Joshua Balogun <joshua.balogun@kominiti.net>
2022-09-13 14:24:12 +0100

    commit for mvc completed

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -6,21 +6,30 @@ exports.getAddProduct = (req, res, next) => {
     path: "/admin/add-product",
     activeAddProduct: true,
     productCSS: true,
-    activeAddProduct: true,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const n = req.body.price;
+  // const num = n.toFixed(2);
+  // console.log(num);
+  const product = new Product(
+    req.body.title,
+    req.body.price
+  );
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
-    prods: products,
-    docTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
+  const products = Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      docTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+    });
   });
-}
+
+};
