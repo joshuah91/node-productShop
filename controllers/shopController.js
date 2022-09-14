b5a8b6395f35fcaf318588f366afd4ce49b88574
Joshua Balogun <joshplexus8@gmail.com>
2022-09-14 23:28:45 +0100

    x

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -2,15 +2,7 @@ const Product = require('../models/product');
 
 
 
-exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      docTitle: "All Products",
-      path: "/products",
-    });
-  });
-};
+
 
 exports.getIndex = (req, res, next) => {
   const products = Product.fetchAll((products) => {
