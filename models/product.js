b73bfb9dfb6d0c3ce4c0c7e9bc18eafe1ad7ee77
Joshua Balogun <joshua.balogun@kominiti.net>
2022-09-19 11:49:06 +0100

    commit for product-list view

--- models/product.js
+++ models/product.js
@@ -25,6 +25,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -38,5 +39,10 @@ module.exports = class Product {
     getProductsFromFile(cb);
   }
 
-
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
 };
