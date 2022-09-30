009f165f0a7f09da127332a6d27577ede1cb5847
Joshua Balogun <joshua.balogun@kominiti.net>
2022-09-30 12:42:06 +0100

    mysql setup complete

--- routes/shop.js
+++ routes/shop.js
@@ -18,8 +18,10 @@ router.post("/cart", shopController.postCart);
 
 router.post("/cart-delete-item", shopController.postCartDeleteItem);
 
+router.post("/create-order", shopController.postOrder);
+
 router.get("/orders", shopController.getOrders);
 
-router.get("/checkout", shopController.getCheckOut);
+// router.get("/checkout", shopController.getCheckOut);
 
 module.exports = router;
