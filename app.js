e029512167a904beb4d345f64f768887420b1fa9
Joshua Balogun <joshplexus8@gmail.com>
2023-02-04 20:35:36 +0100

    commit for login

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongodbStore = require("connect-mongodb-session")(session)
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const store = new MongodbStore({
+  uri: process.env.MONGO_URI,
+  collection: 'sessions'
+})
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const authRoutes = require("./routes/auth");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my-secret", resave: false, saveUninitialized: false, store: store })
+);
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
+app.use(authRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://joshuah91:JOSEphine@cluster0.5ppqk2h.mongodb.net/test'
-  )
-  .then(result => {
-    User.findOne().then(user => {
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: 'Josh',
-          email: 'email@test.com',
+          name: "Josh",
+          email: "email@test.com",
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
     });
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
